24ced082656b9662c504ae3f3754dad3578acb80
walter.craig <14223976+waltercraig@users.noreply.github.com>
2024-03-19 23:31:02 +0000

    added wordcount function

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 def main():
     book_path = 'books/frankenstein.txt'
     book_text = read_book(book_path)
-    print(book_text)
+    word_count = count_words(book_text)
+    print(f"{word_count} words found in the document")
 
+def count_words(string):
+    # split the string
+    split_string = string.split()
+    # get the length of the array
+    return len(split_string)
 
 def read_book(path):
     with open(path) as f:
