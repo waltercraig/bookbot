1e0225abdb2fec9a9dc3026989a5acbd637bff4f
walter.craig <14223976+waltercraig@users.noreply.github.com>
2024-03-27 00:36:20 +0000

    updated

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ def main():
     book_path = 'books/frankenstein.txt'
     book_text = read_book(book_path)
     word_count = count_words(book_text)
-    print(f"{word_count} words found in the document")
+    character_c = character_count(book_text)
+    # print(f"{word_count} words found in the document")
+    # for i in len(character_c):
+    #     print(character_c[i])
+    # print(character_c)
+    print(len(character_c))
 
 def count_words(string):
     # split the string
@@ -10,8 +15,21 @@ def count_words(string):
     # get the length of the array
     return len(split_string)
 
+def character_count(string):
+    character_list = {}
+    for i in string:
+        if i != ' ':
+            x = i.lower()
+            if x not in character_list:
+                character_list[x] = 1
+            else:
+                character_list[x] += 1
+    return character_list
+
 def read_book(path):
     with open(path) as f:
         return f.read()
 
+
+# character_count()
 main()
